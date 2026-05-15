e267d92160079c017ff3848b38fac2099c541c5d
nutcas3 <mauricee423@gmail.com>
2026-05-15 11:18:09 +0300

    feat: add gRPC server with graceful shutdown and keepalive configuration for Crossplane function runner
    Implement main() in cmd/function/main.go with FUNCTION_LISTEN_ADDRESS environment variable defaulting to :9443, create TCP listener with net.Listen(), configure grpc.NewServer() with keepalive parameters (5m MaxConnectionIdle/2m Time/20s Timeout), register FunctionRunnerServiceServer with NewFunction(), add signal handling for SIGINT/SIGTERM triggering GracefulStop(), start server with Serve() and error

--- cmd/function/main.go
+++ cmd/function/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"the-engine/internal/function"
 
 	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 type Function struct {
@@ -21,4 +29,42 @@ func NewFunction() *Function {
 
 func (f *Function) RunFunction(ctx context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
 	return f.handler.RunFunction(ctx, req)
+}
+
+func main() {
+	addr := os.Getenv("FUNCTION_LISTEN_ADDRESS")
+	if addr == "" {
+		addr = ":9443"
+	}
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen on %s: %v", addr, err)
+	}
+	defer lis.Close()
+
+	opts := []grpc.ServerOption{
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle: 5 * time.Minute,
+			Time:              2 * time.Minute,
+			Timeout:           20 * time.Second,
+		}),
+	}
+
+	grpcServer := grpc.NewServer(opts...)
+	fnv1beta1.RegisterFunctionRunnerServiceServer(grpcServer, NewFunction())
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-shutdown
+		log.Printf("Received signal %s, initiating graceful shutdown", sig)
+		grpcServer.GracefulStop()
+	}()
+
+	log.Printf("Function runner gRPC server listening on %s", addr)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("gRPC server encountered error: %v", err)
+	}
 }
